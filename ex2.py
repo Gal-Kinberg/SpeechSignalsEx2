5769f90ced4842d53e86e41a24dc7c45b8bce930
TKratter <tomkratter@gmail.com>
2022-12-14 00:28:35 +0200

    added argparse + write to file, has some bug, fix in later version

--- ex2.py
+++ ex2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import argparse
 
 
 def CTCLoss(network_output_path: str, input_labels: str, alphabet: str) -> float:
@@ -24,17 +25,23 @@ def CTCLoss(network_output_path: str, input_labels: str, alphabet: str) -> float
     # use dynamic programing to calculate alpha
     for t in range(1, T):
         for s in range(L):
-            additive_term = alpha[t-1, s]
+            additive_term = alpha[t - 1, s]
             if s > 0:
-                additive_term += alpha[t-1, s-1]
-            if (s > 1) and (z[s] != '-') and (z[s] != z[s-2]):
-                additive_term += alpha[t-1, s-2]
+                additive_term += alpha[t - 1, s - 1]
+            if (s > 1) and (z[s] != '-') and (z[s] != z[s - 2]):
+                additive_term += alpha[t - 1, s - 2]
             alpha[t, s] = additive_term * y_probability_matrix[t, alphabet_dict[z[s]]]
 
     return np.round(alpha[-1, -2] + alpha[-1, -1], 2)
 
-    # write results to file
-
 
 if __name__ == "__main__":
-    print(CTCLoss("mat1.npy", "a", "ab"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("network_output_path", type=str, help="Path to the network output matrix")
+    parser.add_argument("input_labels", help="input labels")
+    parser.add_argument("alphabet", help="alphabet")
+    args = parser.parse_args()
+    result = CTCLoss(args.network_output_path, args.input_labels, args.alphabet)
+    # write result to file
+    with open("output.txt", "w") as f:
+        f.write(str(result))
